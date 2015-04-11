df343e2bf409f4e313ef1ebe278cc755f3803449
joe <joe@debian>
2015-04-11 18:44:27 -0400

    Updated deploy script for working with multiple machines

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,15 @@
 # Deploy Blog
 # Make sure all posts have been added to git and committed first!
 
+set -e
+
+# Make sure master is synced
+git checkout master
+git pull
+git push origin master
+
 # Make sure source is synced
+git checkout source
 git pull
 git push origin source
 
