1b1cd720b66f315dc0ce4626873febe61bce6f60
joe <joe@debian>
2015-04-11 20:32:32 -0400

    Site updated at 2015-04-12 00:32:32 UTC

--- deploy.sh
+++ deploy.sh
@@ -4,29 +4,9 @@
 
 set -e
 
-# Make sure master is synced
-git checkout master
-git pull
-git push origin master
-
-# Make sure source is synced
-git checkout source
-git pull
-git push origin source
-
-# Make sure deployment is synced
-cd _deploy/
-git pull
-cd -
-
 # Deploy
 rake _0.9.2.2_ generate
 rake _0.9.2.2_ deploy
 
 # Resync to prevent problems later
-git pull
-
-cd _deploy/
-git pull
-cd -
-
+./sync.sh
