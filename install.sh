f3ee0c9c759f596aad4b7de539ee96812d2707d5
joe <joe@debian>
2015-04-19 15:40:39 -0400

    Updated scripts

--- install.sh
+++ install.sh
@@ -3,4 +3,11 @@
 sudo apt-get install ruby bundler git ruby-gsl
 sudo gem install rake --version 0.9.2.2
 bundle install
+
 rake _0.9.2.2_ setup_github_pages
+
+cd _deploy/
+git pull origin master
+git add index.html
+git commit -m 'Deployment'
+
--- sync.sh
+++ sync.sh
@@ -4,11 +4,6 @@
 
 set -e
 
-# Make sure master is synced
-git checkout master
-git pull
-git push origin master
-
 # Make sure source is synced
 git checkout source
 git pull
